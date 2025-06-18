573db6d642b358fd050800bd74e9a1df364be64b
Nathan Gorga <gorga.nathan05@gmail.com>
2025-06-18 16:21:24 +0200

    main.cpp code skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,37 @@
 
 
 int main(void){
+    //// comments in capital letters refer to particular stages in the architecture diagram
+
+    //INIT BOARD
+
+
+    //INIT UART
+
+
+    //INIT BUFFERS
+
+
+    //HEARTBEAT TO OPENBCI
+
+
+    //LAUNCH DATA INTAKE THREAD
+
+
+    //LAUNCH DATA PROCESSING THREAD
+
+
+    //WAIT FOR DATA PROCESSING TO RESPOND
+
+
+    //send ACK signal once the thread responds
+
+
+    //WAIT FOR DATA INTAKE TO RESPOND
+
+    //send ACK signal once the thread responds
+
+    //TERMINATE MASTER THREAD
+
     return 0;
 }
